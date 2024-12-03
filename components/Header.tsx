a92727e3cedccc53216cc9095cf07df9081e2e20
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-12-03 10:51:05 +0100

    fixing header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { FaHeart } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <section className="w-full px-2 pt-2 flex justify-between">
+    <section className="w-full p-2 flex justify-between fixed z-10 bg-black">
       <Link href={"/"}>
         <h1 className="md:text-xl lg:text-2xl">Dan&apos;s movies</h1>
       </Link>
