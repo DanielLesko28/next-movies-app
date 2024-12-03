629d0eaa2b79d079f77f5ff20983515b856d0cb9
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-12-03 19:11:12 +0100

    shuffling movies

--- utils/helperFunctions.ts
+++ utils/helperFunctions.ts
@@ -14,3 +14,11 @@ export const roundToOneDecimalPlace = (num: number): number => {
 export const trimTitle = (title: string, maxLength: number = 22) => {
   return title.length > maxLength ? `${title.slice(0, maxLength)}...` : title;
 };
+
+export const shuffle = (array: any) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
